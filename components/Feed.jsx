f70c2cb6139a276f982ed0e4d5a3bd4e536683a1
Varun Sharma <varun.1822en1163@kiet.edu>
2023-07-05 00:56:33 +0530

    3rd commit

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,28 +2,27 @@
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
-const PromptCardList = ({ data, handleTagClick }) => {
+const PromptCardList = ({ data }) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
         <PromptCard
           key={post._id}
           post={post}
-          handleTagClick={handleTagClick}
         />
       ))}
     </div>
   );
 };
 const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
   const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState([]);
   const handleSearchChange = (e) => {};
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
-      setPosts(data);
+      setAllPosts(data);
     };
     fetchPosts();
   }, []);
@@ -40,7 +39,7 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      <PromptCardList data={allPosts} />
     </section>
   );
 };
